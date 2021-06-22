b14eb37c97f3c72553f58d4cdf4cc97cb9851eda
pablo <treendaa@gmail.com>
2021-06-22 20:09:05 +0200

    added initial reservation and payment tests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -37,40 +37,40 @@ db.on("disconnected", function () {
 });
 connect();
 
-let seats = new Map();
-seats.set("A", {
-	numberOfSeats: 10,
-	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	reservedSeats: [],
-});
-seats.set("B", {
-	numberOfSeats: 10,
-	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	reservedSeats: [],
-});
-seats.set("C", {
-	numberOfSeats: 10,
-	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	reservedSeats: [],
-});
-seats.set("D", {
-	numberOfSeats: 10,
-	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	reservedSeats: [],
-});
+// let seats = new Map();
+// seats.set("A", {
+// 	numberOfSeats: 10,
+// 	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+// 	reservedSeats: [],
+// });
+// seats.set("B", {
+// 	numberOfSeats: 10,
+// 	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+// 	reservedSeats: [],
+// });
+// seats.set("C", {
+// 	numberOfSeats: 10,
+// 	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+// 	reservedSeats: [],
+// });
+// seats.set("D", {
+// 	numberOfSeats: 10,
+// 	aisleSeats: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+// 	reservedSeats: [],
+// });
 
-const eventId = new mongoose.Types.ObjectId();
-const event = {
-	_id: eventId,
-	name: "Huge Concert",
-	seatInfo: seats,
-	ticketPrice: 30,
-};
+// const eventId = new mongoose.Types.ObjectId();
+// const event = {
+// 	_id: eventId,
+// 	name: "Huge Concert",
+// 	seatInfo: seats,
+// 	ticketPrice: 30,
+// };
 
-const seedDBwithEvent = async () => {
-	await Event.deleteMany();
-	const newEvent = await new Event(event).save();
-	// Logging out the id for testing purposes
-	console.log(newEvent._id);
-};
-seedDBwithEvent();
+// const seedDBwithEvent = async () => {
+// 	await Event.deleteMany();
+// 	const newEvent = await new Event(event).save();
+// 	// Logging out the id for testing purposes
+// 	console.log(newEvent._id);
+// };
+// seedDBwithEvent();
